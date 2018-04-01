7263ca80016c35c3e3f79ff78e3b77300d553a83
ramir266 <ramir266@mail.chapman.edu>
2018-03-31 21:25:48 -0700

    Update RunFile.cpp

--- RunFile.cpp
+++ RunFile.cpp
@@ -1,8 +1,9 @@
 #include <fstream>
-#include <string>
 #include <iostream>
+#include <string>
 #include <array>
 #include "RunFile.h"
+//#include "GenQueue.h"
 
 using namespace std;
 
@@ -12,7 +13,6 @@ using namespace std;
 		clock tick
 		number of customers
 		(length of service time X number of customers)
-
 	- Once the number of service windows is determined create the serviceWindow array of type Customer
 	
 	- Then proceed to read the remainder of the file using the algorithm above.
@@ -24,15 +24,18 @@ using namespace std;
 
 RunFile::RunFile()
 {
-	numberOfWindows = 0;
+	numberOfOpenWindows = 0;
 	numberOfStudents = 0;
 	timeOfService = 0;
 	clockTick = 0;
 }
 
 RunFile::~RunFile(){}
+
 void RunFile::read( string inFile )
 {
+	//GenQueue<Customer> *testQueue = new GenQueue<Customer>();
+
 	fstream simFile;
 	string inLine;
 
@@ -40,23 +43,31 @@ void RunFile::read( string inFile )
 
 	// number of windows
 	getline( simFile, inLine );
-	numberOfWindows = stoi( inLine );
-	cout << "Number of windows: " << numberOfWindows << endl;
+	numberOfOpenWindows = stoi(inLine);
+	cout << "Number of windows: " << numberOfOpenWindows << endl;
 
 	// create array of windows
-	serviceWindow = new Customer*[numberOfWindows];				// defined service window array and define limits
+	/*serviceWindow = new Customer*[numberOfWindows];				// defined service window array and define limits
 
 	for ( int i = 0; i < numberOfWindows; ++i )					// create the empty array
 	{
 		Customer* windowCustomer = new Customer();
 		serviceWindow[ i ] = windowCustomer;
+	}*/
+
+	serviceWindows = new int[numberOfOpenWindows];				// defined service window array and define limits
+
+
+	for ( int i = 0; i < numberOfOpenWindows; ++i )					// create the empty array
+	{
+		serviceWindows[ i ] = 0;
 	}
 
 	// loop until eof
 	while (getline( simFile, inLine ))
 	{
 		// get the clock tick
-		clockTick = stoi( inLine );
+		clockTick = stoi(inLine);
 		cout << "clock: " << clockTick << endl;
 
 		// number of students at this arrival time
@@ -71,12 +82,57 @@ void RunFile::read( string inFile )
 			cout << "Time of Service: " << timeOfService << endl;
 			
 			/* build virtual line but first create customer. */
-			//Customer* theCustomer = new Customer();
-			//theCustomer.BuildCustomer( clockTick, timeOfService );
+			Customer* theCustomer = new Customer();
+			theCustomer->BuildCustomer( clockTick, timeOfService );
+			cout<<"This is the specific customer's clock tick: " << theCustomer->timeInLine<<endl;
+			cout<<"This is the specific customer's length of service: "<< theCustomer->timeService<<endl;
+			cout<<"This is the specific customer's waiting time: "<< theCustomer->timeWaiting<<endl;
 
-			/* put the customer into the queue */
-			//theVirtualLine.
+			//putting customers into queue
+			testQueue->enqueue(theCustomer);
+			cout<<testQueue->getSize()<<endl;
+		}
+	}
+}
 
+void RunFile::simulation()
+{
+	int clock = 0;
+	bool finished = false; 
+	int emptyWindows = 0;
+
+	while(finished != true)
+	{
+		clock++;
+		//declaring the intial time
+		cout<<"Clock Tick: "<<clock<<endl;
+		for(int i = 0; i < numberOfOpenWindows; ++i) //array of open windows
+		{
+			if(!testQueue->isEmpty() && serviceWindows[i] == 0) //checking whether queue of students is empty and window vacant
+			{
+				Customer* temp1 = testQueue->dequeue(); //creating a temp customer and assigning it to the cust on front of line
+				if(clock = temp1->timeInLine)
+				{
+					serviceWindows[i] = temp1->timeService; //setting first OPEN window equal to temp1 time needed of service 
+					//cout<<"Window "<<i+1<<" occupied for: "<< serviceWindows[i]<<"\n Student time need: "<< temp1->timeService <<endl;
+				}
+			}
+			else  // what's happening here?
+			{
+				if(serviceWindows[i] == 0)
+				{
+					emptyWindows++;
+				}
+				else
+					serviceWindows[i]--; 
+			}
+			cout<<"Window "<<i+1<<" occupied for: "<< serviceWindows[i] <<endl;
+		}
+
+		if(emptyWindows == numberOfOpenWindows)
+		{
+			finished = true;
+			cout<<"Empty Windows: "<<emptyWindows<<"\nOpen Windows: "<<numberOfOpenWindows<<endl;
 		}
 	}
 
